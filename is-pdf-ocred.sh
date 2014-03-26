3f7c4d4c1f795576c554187ffccc537413731e06
root <root@mappt.swarthmore.edu>
2014-03-26 12:25:40 -0400

    Removed old files

--- is-pdf-ocred.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-MYFONTS=$(pdffonts -l 5 "$1" | tail -n +3 | cut -d' ' -f1 | sort | uniq)
-if [ "$MYFONTS" = '' ] || [ "$MYFONTS" = '[none]' ]; then
-    echo "NOT OCR'ed: $1"
-else 
-    echo "$1 is OCR'ed."
-fi 
